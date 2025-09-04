b53d5d54f62f4fd75cfc7a25b0b547637a25aa7c
RajalakshmiVenkatasamy <rv.rajalakshmivenkatasamy@gmail.com>
2025-09-04 12:37:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,4 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "myterraformstorage123"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+
