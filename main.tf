b90b58bc51db5a264cb7574908e7b46126fd025f
RajalakshmiVenkatasamy <rv.rajalakshmivenkatasamy@gmail.com>
2025-09-05 07:32:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,21 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+     azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
 resource "azurerm_resource_group" "rg" {
   name     = "my-terraform-rg"
   location = var.location
 }
+
+
 
 
 
