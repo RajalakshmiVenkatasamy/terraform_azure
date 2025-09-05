4425bbf949ad1f20c57b93d23f50b408cd3802ca
RajalakshmiVenkatasamy <rv.rajalakshmivenkatasamy@gmail.com>
2025-09-05 00:20:06 -0400

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {  
-    resource_group_name  = "terraform-RG"  
-    storage_account_name = "terraformstorage23"  
-    container_name       = "storagecontainer"    
-    key                  = "terraform.tfstate"
-  }
-}
