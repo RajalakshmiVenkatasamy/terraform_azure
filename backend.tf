c3da6d5ddb337842be9f4608d83ef2998d3fc5fa
RajalakshmiVenkatasamy <rv.rajalakshmivenkatasamy@gmail.com>
2025-09-04 23:52:07 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,15 +1,8 @@
 terraform {
-  backend "azurerm" {
-    # Resource group that contains the storage account for Terraform state
-    resource_group_name  = "terraform-RG"
-
-    # Storage account name
-    storage_account_name = "terraformstorage23"
-
-    # Container name inside the storage account
-    container_name       = "storagecontainer"
-
-    # The state file name
+  backend "azurerm" {  
+    resource_group_name  = "terraform-RG"  
+    storage_account_name = "terraformstorage23"  
+    container_name       = "storagecontainer"    
     key                  = "terraform.tfstate"
   }
 }
