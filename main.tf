b7c6412dde85b186c697b19cc2214e24b8c17be0
RajalakshmiVenkatasamy <rv.rajalakshmivenkatasamy@gmail.com>
2025-09-06 14:00:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,52 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "my-terraform-rg"
+  location = var.location
+}
+
+# App Service Plan with tags
+resource "azurerm_app_service_plan" "asp" {
+  name                = "my-app-service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  kind     = "App"      
+  reserved = false      
+
+  sku {
+    tier = "Basic"     
+    size = "B1"
+  }
+
+  tags = {
+    Environment = "Dev"
+    Project     = "TerraformDemo"
+  }
+}
+
 
 
 
