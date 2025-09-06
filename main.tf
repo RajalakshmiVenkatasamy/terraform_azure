c067065e02933855e92c01862b1762e03374df7c
RajalakshmiVenkatasamy <rv.rajalakshmivenkatasamy@gmail.com>
2025-09-06 13:12:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,19 +15,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "myterraformstorage23"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
 
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "myterraformcontainer"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
-}
 
 
 
